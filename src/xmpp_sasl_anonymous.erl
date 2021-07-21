8025f250a157cae31fc05c16f483fcecab0d4374
tu-nv <vantu.bkhn@gmail.com>
2021-07-21 01:28:39 +0000

    move xmpp_sasl_anonymous to xmpp repo

--- src/xmpp_sasl_anonymous.erl
+++ src/xmpp_sasl_anonymous.erl
@@ -20,16 +20,34 @@
 -behaviour(xmpp_sasl).
 -protocol({xep, 175, '1.2'}).
 
--export([mech_new/6, mech_step/2]).
+-export([mech_new/6, mech_step/2, format_error/1]).
 
--record(state, {server = <<"">> :: binary()}).
+-include_lib("xmpp.hrl").
 
-mech_new(_Mech, _Socket, Host, _GetPassword, _CheckPassword, _CheckPasswordDigest) ->
-    #state{server = Host}.
+-record(state, {ws_pid :: binary(), host :: binary()}).
 
-mech_step(#state{}, _ClientIn) ->
+-type error_reason() :: not_authorized.
+-export_type([error_reason/0]).
+
+-spec format_error(error_reason()) -> {atom(), binary()}.
+format_error(not_authorized) ->
+    {'not-authorized', <<"Invalid jitsi jwt token">>}.
+
+mech_new(_Mech, Socket, Host, _GetPassword, _CheckPassword, _CheckPasswordDigest) ->
+    % TODO: a better method to extract WsPid
+    WsPid = element(2, element(3, Socket)),
+    #state{ws_pid = WsPid, host = Host}.
+
+mech_step(#state{ws_pid = WsPid, host = Host}, _ClientIn) ->
+    JWTResult = ejabberd_hooks:run_fold(check_jwt_token, Host, ok, [WsPid, Host]),
+
     User = iolist_to_binary([p1_rand:get_string(),
                              integer_to_binary(p1_time_compat:unique_integer([positive]))]),
-    {ok, [{username, User},
-	  {authzid, User},
-	  {auth_module, anonymous}]}.
+
+    if JWTResult == ok ->
+        {ok, [{username, User},
+        {authzid, User},
+        {auth_module, anonymous}]};
+    true ->
+        {error, not_authorized, User}
+    end.
